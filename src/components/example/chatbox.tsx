80dc9c0c9e8af02dfd210ee1ee5f7986f4a9bd04
Enkhbold470 <enkhbold470@gmail.com>
2024-08-24 01:30:05 -0700

    space

--- src/components/example/chatbox.tsx
+++ src/components/example/chatbox.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import React, { useState, useEffect } from "react";
 import { BackgroundGradient } from "../ui/background-gradient";
 import { IconSend } from "@tabler/icons-react";
